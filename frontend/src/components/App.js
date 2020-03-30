a84a2c717f1c6cbf973601cfb87dbd2f3e6f0e41
Jason Cheng <jasoncheng20@gmail.com>
2020-03-30 14:55:14 -0700

    url-loader to get gif working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,9 +4,8 @@ import { render } from 'react-dom';
 import Pomodoro from './Pomodoro.js';
 import ShortBreak from './ShortBreak.js';
 import LongBreak from './LongBreak.js';
-// import tomato from '../assets/tomato_test.png'
+import tomato from '../assets/Tomato.gif'
 import QuestList from './QuestList.js';
-// img className = "tomato" src = {tomato}/
 export default class App extends Component {
     constructor(props){
         super(props);
@@ -56,6 +55,7 @@ export default class App extends Component {
                 {showPomodoro && <Pomodoro/>}
                 {showShortBreak && <ShortBreak/>}
                 {showLongBreak && <LongBreak/>}
+                <img className = "tomato" src = {tomato}/>
                 <QuestList/>
             </div>
         )
